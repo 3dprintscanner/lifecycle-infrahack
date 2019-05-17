acf7e2625099b336c5d84cfa6a44145657d087f1
3dprintscanner <1988anthonythomas@gmail.com>
2019-05-17 21:59:42 +0100

    add api controller with permit params

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,11 @@ Rails.application.routes.draw do
   get '/overview', to: 'overviews#index'
   root :controller => 'overviews', :action => '/' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, :format => :json do
+    namespace :v1 do
+      get  '/chargedata', to: 'chargedata#index'
+      post  '/submitdata', to: 'chargedata#submitdata'
+    end
+  end
 end
